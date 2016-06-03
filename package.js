6ce27fb54c703e4796f141aa41ad324555d90af8
Aram Bayadyan <aram.bv@gmail.com>
2016-06-03 05:29:36 +0400

    moved to the latest version of intro.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'aramix:introjs',
-  version: '1.0.0',
+  version: '2.1.0',
   // Brief, one-line summary of the package.
   summary: 'Meteor package for intro.js with improvements',
   // URL to the Git repository containing the source code for this package.
